fda8319f7134f55a9b05fdb5dcfd78cedb650443
aconcia1 <conciatorianthony@gmail.com>
2019-05-12 23:27:23 -0400

    Adding more objects to app

--- JumpNShootWoMan/JumpNShootWoMan/GameScene.swift
+++ JumpNShootWoMan/JumpNShootWoMan/GameScene.swift
@@ -9,32 +9,37 @@
 //awefafsrgsthdfjdyjdfhj
 //aeferfesgrthdhdhrtherg
 //afhokjsfouoasidfhojhdsfgojh
+//iijdokjhawodkjhaowdkhjawodkjh
+//asrfglankjshefkjhaserfkljhsdgarfkj
+//aseojhfdoksaejhfklsajefhsalkejhfgikjh
+//asefkjhsefkjhasefkjhaseklfjhasertfokj
+//spekfjhokjhoikjhqaiwhjjgejkhgrf
 
 import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+
     private var label : SKLabelNode?
     private var spinnyNode : SKShapeNode?
-    
+
     let samus = SKSpriteNode(imageNamed:"placeholder")
     let aButton = SKSpriteNode(imageNamed:"a-button")
     let bButton = SKSpriteNode(imageNamed:"b-button")
     let rButton = SKSpriteNode(imageNamed:"r-button")
     let lButton = SKSpriteNode(imageNamed:"l-button")
     let imageName = "backgroundCave"
-    
+
     override func didMove(to view: SKView) {
-        
+
         view.layer.contents = UIImage(named: "backgroundCave")?.cgImage
     }
-    
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        
-        
+
+
     }
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
